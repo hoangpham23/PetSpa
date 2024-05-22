18787fc4aa6f93c37eaf3fb0f80711da1ab48587
aziky <pcm230304@gmail.com>
2024-05-22 20:24:26 +0700

    sign in and sign up

---
+++ src/main/java/com/team/controller/SignUpController.java
@@ -0,0 +1,43 @@
+package com.team.controller;
+
+
+import com.team.model.Account;
+import com.team.model.Customer;
+import com.team.service.AccountService;
+import com.team.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sign-up")
+public class SignUpController {
+
+    private CustomerService customerService;
+    private AccountService accountService;
+
+    public SignUpController(CustomerService customerService, AccountService accountService) {
+        this.customerService = customerService;
+        this.accountService = accountService;
+    }
+
+
+    @PostMapping("")
+    public ResponseEntity<?> signUp(
+            @RequestParam String customerName,
+            @RequestParam String password,
+            @RequestParam String phoneNumber,
+            @RequestParam String email)
+    {
+        if (accountService.checkEmail(email)){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email is already in use");
+        }
+        accountService.createAccount(email, password);
+        int customerId = accountService.findAccountID(email, password).getAccountID();
+
+        Customer customer = customerService.createCustomer(customerId, customerName, phoneNumber, email);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+    }
+
+}
---
+++ src/main/java/com/team/model/Customer.java
@@ -0,0 +1,23 @@
+package com.team.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "CUSTOMERS")
+public class Customer {
+
+    @Id
+    private int customerID;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+    private int numberOfPets;
+}
+ 
