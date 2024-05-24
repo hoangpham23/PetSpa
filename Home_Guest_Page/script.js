5136abc3417c43d950b8f390a057bacca0cbbc12
Hoàng Phạm <108130949+hoangpham23@users.noreply.github.com>
2024-05-24 09:42:15 +0700

    Delete Home_Guest_Page directory

--- Home_Guest_Page/script.js
+++
@@ -1,5 +0,0 @@
-/*============= sticky navbar =============*/ 
-window.onscroll = () => {
-    let header = document.querySelector('.header')
-    header.classList.toggle('sticky', window.scrollY >100);
-    }
