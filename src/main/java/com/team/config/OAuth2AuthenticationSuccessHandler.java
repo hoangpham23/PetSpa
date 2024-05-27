f2b7411dd781f7401133a8b6e10058e928c4fd52
aziky <pcm230304@gmail.com>
2024-05-27 22:50:52 +0700

    login with google

--- src/main/java/com/team/config/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/team/config/OAuth2AuthenticationSuccessHandler.java
@@ -1,52 +1,57 @@
-//package com.team.config;
-//
-//
-//
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.servlet.http.HttpSession;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-//import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-//import org.springframework.stereotype.Component;
-//import java.io.IOException;
-//
-//import java.util.Map;
-//
-//@Component
-//public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
-//
-//
-//    private final HttpSession httpSession;
-//
-//    public OAuth2AuthenticationSuccessHandler(HttpSession httpSession) {
-//        this.httpSession = httpSession;
-//    }
-//
-//    @Override
-//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-//                                        Authentication authentication) throws IOException, ServletException {
-//        if (authentication instanceof OAuth2AuthenticationToken) {
-//            OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-//            Map<String, Object> attributes = oauthToken.getPrincipal().getAttributes();
-//
-//            // Retrieve email and name from OAuth2 user attributes
-//            String email = (String) attributes.get("email");
-//            // check email that exist or not
-//            // if exist response back to sign-in and say already exist email.
-//            System.out.println("email: " + email);
-//            ResponseEntity<String> responseEntity = ResponseEntity.ok("{\"email\":\"" + email + "\"}");
-//            response.setStatus(responseEntity.getStatusCodeValue());
-//            response.setContentType("application/json");
-//            response.getWriter().write(responseEntity.getBody());
-//            httpSession.setAttribute("EMAIL", email);
-//            // Redirect to sign-up page after successful OAuth2 authentication
-//            response.sendRedirect("http://localhost:3000/sign-up");
-//        } else {
-//            // Handle other types of authentication
-//            super.onAuthenticationSuccess(request, response, authentication);
-//        }
-//    }
-//}
+package com.team.config;
+
+
+
+import com.team.service.AccountService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+import java.util.Map;
+
+@Component
+public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+
+    private final HttpSession httpSession;
+    private final AccountService accountService;
+
+    public OAuth2AuthenticationSuccessHandler(HttpSession httpSession, AccountService accountService) {
+        this.httpSession = httpSession;
+        this.accountService = accountService;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+            Map<String, Object> attributes = oauthToken.getPrincipal().getAttributes();
+
+            // Retrieve email and name from OAuth2 user attributes
+            String email = (String) attributes.get("email");
+            // check email that exist or not
+            // if exist response back to sign-in and say already exist email.
+            if (accountService.checkEmail(email)){
+                response.setStatus(HttpServletResponse.SC_CONFLICT);
+                response.setContentType("application/json");
+                response.getWriter().write("{\"error\": \"Email already exists\"}");
+                response.getWriter().flush();
+                response.sendRedirect("http://localhost:3000/sign-in2");
+                return;
+            }
+            // Redirect to sign-up page after successful OAuth2 authentication
+            response.sendRedirect("http://localhost:3000/home-page");
+        } else {
+            // Handle other types of authentication
+            super.onAuthenticationSuccess(request, response, authentication);
+        }
+    }
+}
--- src/main/java/com/team/config/SecurityConfig.java
+++ src/main/java/com/team/config/SecurityConfig.java
@@ -1,29 +1,31 @@
-//package com.team.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler) throws Exception {
-//        return http
-//                .authorizeHttpRequests(auth -> {
-//                    auth.requestMatchers("/home-page/", "/sign-in", "/forgotpassword/").permitAll(); // Allow access to these URLs without authentication
-//                    auth.anyRequest().authenticated(); // All other requests require authentication
-////                    auth.requestMatchers("/home-page").authenticated();
-////                    auth.anyRequest().permitAll();
-//                })
-//                .oauth2Login(oauth ->
-//                        oauth.loginPage("http://localhost:3000/sign-in")
-//                                .successHandler(oAuth2AuthenticationSuccessHandler)
-//                )
-//                .build();
-//    }
-//}
+package com.team.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/home-page/", "/sign-in", "/forgotpassword/").permitAll(); // Allow access to these URLs without authentication
+                    auth.anyRequest().authenticated(); // All other requests require authentication
+                })
+                // change the default page login with google
+                .oauth2Login(oauth ->
+                        oauth.loginPage("http://localhost:3000/sign-in2")
+                                .successHandler(oAuth2AuthenticationSuccessHandler)
+                                // successful login with google redirect to home-page
+                )
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
