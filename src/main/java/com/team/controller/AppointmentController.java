b616135e47abb6a91d00817a9faae3be7de2db8b
aziky <pcm230304@gmail.com>
2024-05-31 11:44:10 +0700

    return appointment

--- src/main/java/com/team/controller/AppointmentController.java
+++ src/main/java/com/team/controller/AppointmentController.java
@@ -5,33 +5,31 @@ import com.team.dto.AppointmentDTO;
 import com.team.service.AppointmentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.ArrayList;
+import java.time.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/book")
-public class BookingController {
+@RequestMapping("/appointment")
+public class AppointmentController {
 
-    private final Logger logger = LoggerFactory.getLogger(BookingController.class);
+    private final Logger logger = LoggerFactory.getLogger(AppointmentController.class);
     private final AppointmentService appointmentService;
 
-    public BookingController(AppointmentService appointmentService) {
+    public AppointmentController(AppointmentService appointmentService) {
         this.appointmentService = appointmentService;
     }
 
     @GetMapping("")
     public ResponseEntity<?> getAllBookings() {
         try {
-            List<AppointmentDTO> appointments = appointmentService.getAllAppointments();
+            LocalDateTime time = LocalDateTime.now().plusDays(1);
+            List<AppointmentDTO> appointments = appointmentService.getAllAppointments(time);
             return ResponseEntity.ok(appointments);
         }catch (Exception e){
             logger.error(e.getMessage());
