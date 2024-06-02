8f130e3a318bb3784cde10004e4253045afb6739
TranQuocMinh <minhtqse183954@fpt.edu.vn>
2024-06-02 17:09:32 +0700

    Service's Page and Service's Details

---
+++ src/main/java/com/team/controller/ServicePageController.java
@@ -0,0 +1,54 @@
+package com.team.controller;
+
+import com.team.dto.ServiceImageDTO;
+import com.team.dto.ServicePageDTO;
+import com.team.service.ServiceImagesService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/service-page")
+public class ServicePageController {
+    private final ServiceImagesService serviceImagesService;
+    private final Logger logger = LoggerFactory.getLogger(ServicePageController.class);
+
+    public ServicePageController(ServiceImagesService serviceImagesService) {
+        this.serviceImagesService = serviceImagesService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllImages() {
+        try {
+            List<ServiceImageDTO> images = serviceImagesService.getHomePageImages();
+            return new ResponseEntity<>(images, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error in getting all images", e);
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{serviceName}")
+    public ResponseEntity<?> serviceDetails(@PathVariable String serviceName) {
+        try {
+            Optional<ServicePageDTO> serviceDetails = serviceImagesService.getServiceDetailsByName(serviceName);
+            if (serviceDetails.isPresent()) {
+                return new ResponseEntity<>(serviceDetails.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            logger.error("Error in getting service details for name: " + serviceName, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- src/main/java/com/team/repository/ServicesImagesRepository.java
+++ src/main/java/com/team/repository/ServicesImagesRepository.java
@@ -3,6 +3,9 @@ package com.team.repository;
 import com.team.model.ServiceImages;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ServicesImagesRepository extends JpaRepository<ServiceImages, Integer> {
 
+    List<ServiceImages> findByServiceID(Integer serviceID);
 }
