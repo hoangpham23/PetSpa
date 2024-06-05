63f1fcfb8d841dd660de8b60e0126d33f67b607d
aziky <pcm230304@gmail.com>
2024-06-05 15:24:02 +0700

    create account and customer dto

---
+++ src/main/java/com/team/dto/AccountDTO.java
@@ -0,0 +1,17 @@
+package com.team.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountDTO {
+
+    private Integer accountID;
+    private String email;
+    private String role;
+
+}
--- src/main/java/com/team/service/CustomerService.java
+++ src/main/java/com/team/service/CustomerService.java
@@ -1,9 +1,8 @@
 package com.team.service;
 
-import com.team.dto.EditAccountDTO;
+import com.team.dto.CustomerDTO;
 import com.team.model.Accounts;
 import com.team.model.Customers;
-import com.team.repository.AccountRepository;
 import com.team.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,16 +12,14 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final AccountService accountService;
-    private final AccountRepository accountRepository;
 
-    public CustomerService(CustomerRepository customerRepository, AccountService accountService, AccountRepository accountRepository){
+    public CustomerService(CustomerRepository customerRepository, AccountService accountService){
         this.customerRepository = customerRepository;
         this.accountService = accountService;
-        this.accountRepository = accountRepository;
     }
 
     @Transactional
-    public Customers createCustomer(String customerName, String phoneNumber, String email, String password) {
+    public CustomerDTO createCustomer(String customerName, String phoneNumber, String email, String password) {
         // create an account first then create a customer
         Accounts accounts = accountService.createAccount(email, password);
         Customers customers = new Customers();
@@ -30,27 +27,17 @@ public class CustomerService {
         customers.setPhoneNumber(phoneNumber);
         customers.setEmail(email);
         customers.setAccounts(accounts);
-        return customerRepository.save(customers);
-    }
-    public boolean checkPhoneNumber(String phoneNumber) {
-        return customerRepository.existsAccountByPhoneNumber(phoneNumber);
+        customers.setNumberOfPets(0);
+        return convertToCustomerDTO(customerRepository.save(customers));
     }
 
-
-    public EditAccountDTO editCustomer(Integer customerID, String customerName, String email, String phoneNumber) {
-        Customers customers = customerRepository.findById(customerID).get();
-        Accounts accounts = accountRepository.findById(customerID).get();
-        customers.setCustomerName(customerName);
-        customers.setEmail(email);
-        accounts.setEmail(email);
-        customers.setPhoneNumber(phoneNumber);
-        customerRepository.save(customers);
-        accountRepository.save(accounts);
-        EditAccountDTO dto = new EditAccountDTO();
+    private CustomerDTO convertToCustomerDTO(Customers customers) {
+        CustomerDTO dto = new CustomerDTO();
+        dto.setCustomerID(customers.getCustomerID());
         dto.setCustomerName(customers.getCustomerName());
-        dto.setEmail(customers.getEmail());
         dto.setPhoneNumber(customers.getPhoneNumber());
-
+        dto.setEmail(customers.getEmail());
+        dto.setNumberOfPets(customers.getNumberOfPets());
         return dto;
     }
 
