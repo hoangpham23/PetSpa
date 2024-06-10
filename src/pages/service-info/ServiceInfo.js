2ac13196bbceef8295b3f3017bb3c96ae99751b2
bibiane-phannh <phuonganhdoan.1303@gmail.com>
2024-06-10 13:39:27 +0700

    Service Information Page

--- src/pages/service-info/ServiceInfo.js
+++ src/pages/service-info/ServiceInfo.js
@@ -1,11 +1,12 @@
 import HeaderForCus from "../../components/header/header-customer";
-import "./ServiceInfor_style.css";
+import style from "./ServiceInfor_style.module.css";
 import "./ServiceInfo_script";
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import axios from "axios";
 import { useService } from "./ServiceContext";
+import service1 from "../../assets/img/service1.jpg"
 // từ trang này nhấn vô phải xem xét số lượng thú của khác hàng, >=1, chuyển sang trang choose Pet, <=1 qua trang insert info cho pet
 function ServiceInfo() {
   let { serviceName } = useParams();
@@ -14,7 +15,7 @@ function ServiceInfo() {
     try {
       console.log(serviceName);
       const response = await axios.get(
-        `http://localhost:8090/home-page/${serviceName}`
+        `http://localhost:8090/home_page/${serviceName}`
       ); // lấy dữ liệu rồi nha
       const responseData = response.data;
       console.log(responseData);
@@ -30,70 +31,61 @@ function ServiceInfo() {
   }, []);
   // làm handle submit cho nút make appointment
   return (
-    <>
+    <> <Helmet><link href='https://unpkg.com/css.gg@2.0.0/icons/css/quote.css' rel='stylesheet'/></Helmet>
       <HeaderForCus />
-      <section className="sectionContainer">
-        <div className="service-div">
-          <div className="service-display">
-            <div className="service-img">
-              <img src="./img/Service1.jpg" alt="" />
+      <section>
+        <div className={style.service_div}>
+            <div className={style.service_display}>
+                <div className={style.service_img}>
+                    <img src={service1} alt=""/>
+                </div>
+                <div className={style.service_price}></div>
             </div>
-            <div className="service-price"></div>
-          </div>
-          <div className="service-content">
-            <h1>INFORMATION</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
-              nesciunt voluptate ut praesentium fugiat quaerat, ab quam dolorum
-              vitae odit expedita recusandae.
-            </p>
-          </div>
-
-          <div className="book">
-            <input type="submit" value="BOOK" className="btn" />
-          </div>
+            <div className={style.service_content}>
+                <h1>INFORMATION</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam nesciunt voluptate ut praesentium fugiat quaerat, ab quam dolorum vitae odit expedita recusandae.</p>
+            </div>
+            <div className={style.book}>
+                <input type="submit" value="BOOK" className="btn"/>
+            </div>
         </div>
-        <div className="feedback-div">
-          <h1>OUR FEEDBACKS</h1>
-          <div className="feedback-display">
-            <div className="service-container">
-              <div className="service-img">
-                <img src="./img/Service1.jpg" alt="" />
-              </div>
-              <p>Service</p>
-              <div className="feedback-content">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias, commodi quaerat optio quisquam tempora corporis
-                  veniam facere, maiores fugiat minus animi architecto.
-                </p>
-              </div>
-            </div>
-            <div className="service-container">
-              <div className="service-img">
-                <img src="./img/Service1.jpg" alt="" />
-              </div>
-              <p>Service</p>
-              <div className="feedback-content">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias, commodi quaerat optio quisquam tempora corporis
-                  veniam facere, maiores fugiat minus animi architecto.
-                </p>
-              </div>
-            </div>
-            <div className="service-container">
-              <div className="service-img">
-                <img src="./img/Service1.jpg" alt="" />
-              </div>
-              <p>Service</p>
-              {/* <!-- <div className="feedback-content">
+  
+        <div className={style.feedback_div}>
+            <h1>OUR FEEDBACKS</h1>
+            <div className={style.feedback_display}>
+                <div className={style.service_container}>
+                    <div className={style.ava_img}>
+                        <img src={service1} alt=""/>
+                    </div>
+                    <i class="gg-quote"></i>
+                    <div className={style.feedback_content}>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, commodi quaerat optio quisquam tempora corporis veniam facere, maiores fugiat minus animi architecto.</p>
-                    </div> --> */}
+                    </div>
+                    <p>[Customer Name]</p>
+                </div>
+                <div className={style.service_container}>
+                    <div className={style.ava_img}>
+                        <img src={service1} alt=""/>
+                    </div>
+                    <i class="gg-quote"></i>
+                    <div className={style.feedback_content}>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, commodi quaerat optio quisquam tempora corporis veniam facere, maiores fugiat minus animi architecto.</p>
+                    </div>
+                    <p>[Customer Name]</p>
+                </div>
+                <div className={style.service_container}>
+                    <div className={style.ava_img}>
+                        <img src={service1} alt=""/>
+                    </div>
+                    <i class="gg-quote"></i>
+                    <div className={style.feedback_content}>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, commodi quaerat optio quisquam tempora corporis veniam facere, maiores fugiat minus animi architecto.</p>
+                    </div>
+                    <p>[Customer Name]</p>
+                </div>
             </div>
-          </div>
         </div>
-      </section>
+    </section>
     </>
   );
 }
