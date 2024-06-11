5c66079161d1f20a086fb46929a0f521e5dc0db0
TranQuocMinh <minhtqse183954@fpt.edu.vn>
2024-06-11 10:14:42 +0700

    Check email and Phone Number for Edit Account

--- src/main/java/com/team/controller/EditAcountController.java
+++ src/main/java/com/team/controller/EditAcountController.java
@@ -3,6 +3,8 @@ package com.team.controller;
 import com.team.dto.EditAccountDTO;
 import com.team.dto.PetDTO;
 import com.team.model.Customers;
+import com.team.repository.CustomerRepository;
+import com.team.service.AccountService;
 import com.team.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +19,11 @@ import java.util.Map;
 @RequestMapping("/edit-account")
 public class EditAcountController {
     private final CustomerService customerService;
+    private final AccountService accountService;
 
-    public EditAcountController(CustomerService customerService) {
+    public EditAcountController(CustomerService customerService, AccountService accountService) {
         this.customerService = customerService;
+        this.accountService = accountService;
     }
 
     @PutMapping
@@ -29,7 +33,12 @@ public class EditAcountController {
         String email = data.get("email");
         String customerName = data.get("customerName");
         String phoneNumber = data.get("phoneNumber");
-
+        if (accountService.checkEmail(email)){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email is already in use");
+        }
+        if (customerService.checkPhoneNumber(phoneNumber)){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Phone number is already in use");
+        }
         EditAccountDTO newInfor = customerService.editCustomer(customerID, customerName, email, phoneNumber);
 
         return ResponseEntity.status(HttpStatus.OK).body(newInfor);
