a7b5bcb62fdbb74de4cb9478cfc2e7484136cbe6
bibiane-phannh <phuonganhdoan.1303@gmail.com>
2024-06-11 11:45:17 +0700

    11/06 code

--- src/pages/choose-pet/ChoosePet.js
+++ src/pages/choose-pet/ChoosePet.js
@@ -1,39 +1,35 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import HeaderForCus from "../../components/header/header-customer";
+import style from "./ChoosePet_style.module.css";
+import { Helmet } from 'react-helmet';
+import { useParams } from 'react-router-dom';
 
 function ChoosePet() {
-  // thiết kế thêm nút + trong trường hợp khách hàng muốn add thêm thú mưới
-  // phần code sign in của back end gửi thêm number of pets
-  // những trang choose pet, choose time,... về sau phải xác thực role customer
-  const [customerID, setCustomerID] = useState("");
-  const [isCustomerIDSent, setIsCustomerIDSent] = useState(false);
-  // chắc chắn phải có customerID
-  useEffect(() => {
-    const customer = JSON.parse(localStorage.getItem("account"));
-    if (customer) {
-      setCustomerID(customer.customerID);
-    }
-  }, []);
-  useEffect(() => {
-    // Gọi handleData chỉ khi customerID đã được gửi đi
-    if (customerID && !isCustomerIDSent) {
-      handleData(customerID);
-      setIsCustomerIDSent(true); // Cập nhật trạng thái đã gửi customerID
-    }
-  }, [customerID, isCustomerIDSent]);
-  async function handleData() {
-    const response = await axios.get("http://localhost:8090/choose-pet", {
-      customerID: customerID,
-    });
-    if (response === 200) {
-      // lưu thông tin mấy con pet ở đây
-      // sau đó in ra
-    }
-  }
-
   return (
     <div>
-      <h1>this is choose pet page</h1>
+      <HeaderForCus />
+      <section>
+        <div className={style.PetInfo_container}>
+          <h1>CHOOSE PET FOR SERVICES</h1>
+          <div className={style.PetInfo_box}>
+            <p>PET 1</p>
+            <p>NAME: </p>
+            <p>WEIGHT (KG): </p>
+          </div>
+          <div className={style.PetInfo_box}>
+            <p>PET 2</p>
+            <p>NAME: </p>
+            <p>WEIGHT (KG): </p>
+          </div>
+          <div className={style.PetInfo_box}>
+            <p>PET 3</p>
+            <p>NAME: </p>
+            <p>WEIGHT (KG): </p>
+          </div>
+        </div>
+        <input type="submit" value="ADD PET" className={style.btn}/>
+      </section>
     </div>
   );
 }
