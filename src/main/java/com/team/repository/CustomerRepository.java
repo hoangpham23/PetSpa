1939dd486e76ea4624a7abf7d5bb956bf94cefe2
TranQuocMinh <minhtqse183954@fpt.edu.vn>
2024-06-11 18:07:23 +0700

    Add Customer Name to Feedback

--- src/main/java/com/team/repository/CustomerRepository.java
+++ src/main/java/com/team/repository/CustomerRepository.java
@@ -4,6 +4,8 @@ import com.team.model.Customers;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends ListCrudRepository<Customers, Integer> {
     Customers findByEmail(String email);
