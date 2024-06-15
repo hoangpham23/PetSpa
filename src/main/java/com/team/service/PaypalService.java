a54524edda8a39accd46a47f4850d9873c51b5a0
aziky <pcm230304@gmail.com>
2024-06-16 00:37:27 +0700

    save payment to database

--- src/main/java/com/team/service/PaypalService.java
+++ src/main/java/com/team/service/PaypalService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @Service
 public class PaypalService {
@@ -64,5 +65,37 @@ public class PaypalService {
         paymentExecution.setPayerId(payerId);
 
         return payment.execute(apiContext, paymentExecution);
+    }
+
+    public Payment createPayment(Map<String, String> data) throws PayPalRESTException {
+        Double total = Double.parseDouble(data.get("amount"));
+        String customerID = data.get("customerID");
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        amount.setTotal(String.format(Locale.forLanguageTag("USD"), "%.2f", total)); // 9.99$ - 9,99€
+
+        Transaction transaction = new Transaction();
+//        transaction.setDescription(description);
+        transaction.setAmount(amount);
+
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod("Paypal");
+
+        Payment payment = new Payment();
+        String customUrl = "?customerID=" + customerID +"&amount=" + total;
+        payment.setIntent("sale");
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl(CANCEL_URL +"?customerID=" + customerID);
+        redirectUrls.setReturnUrl(SUCCESS_URL + customUrl);
+
+        payment.setRedirectUrls(redirectUrls);
+
+        return payment.create(apiContext);
     }
 }
