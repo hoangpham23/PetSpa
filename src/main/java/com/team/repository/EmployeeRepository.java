6f82d7a32fc1bf1b889327c4d7c68de9695db66b
aziky <pcm230304@gmail.com>
2024-06-16 00:36:34 +0700

    search, create, delete employee

--- src/main/java/com/team/repository/EmployeeRepository.java
+++ src/main/java/com/team/repository/EmployeeRepository.java
@@ -4,6 +4,10 @@ import com.team.model.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employees, Integer> {
+
+    List<Employees> findAllByStatus(String status);
 }
