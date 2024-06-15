4e5c42434d704dd0f8d36e6d3bcec1687e784d2a
aziky <pcm230304@gmail.com>
2024-06-15 17:52:03 +0700

    update payment method

--- src/main/java/com/team/service/PaypalService.java
+++ src/main/java/com/team/service/PaypalService.java
@@ -9,14 +9,13 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 @Service
 public class PaypalService {
 
     private final APIContext apiContext;
-    private static final String CANCEL_URL = "http://localhost:8090/payment/cancel";
-    private static final String SUCCESS_URL = "http://localhost:8090/payment/success";
+    private final String CANCEL_URL = "http://localhost:8090/payment/cancel";
+    private final String SUCCESS_URL = "http://localhost:8090/payment/success";
 
     public PaypalService(APIContext apiContext) {
         this.apiContext = apiContext;
@@ -65,43 +64,5 @@ public class PaypalService {
         paymentExecution.setPayerId(payerId);
 
         return payment.execute(apiContext, paymentExecution);
-    }
-
-    public Payment createPayment(Map<String, Object> data) throws PayPalRESTException {
-        Double total = Double.parseDouble(data.get("amount").toString());
-        String customerID = data.get("customerID").toString();
-        List<Integer> listAppointmentID = (List<Integer>) data.get("appointmentIds");
-        Amount amount = new Amount();
-        amount.setCurrency("USD");
-        amount.setTotal(String.format(Locale.forLanguageTag("USD"), "%.2f", total)); // 9.99$ - 9,99€
-
-        Transaction transaction = new Transaction();
-//        transaction.setDescription(description);
-        transaction.setAmount(amount);
-
-        List<Transaction> transactions = new ArrayList<>();
-        transactions.add(transaction);
-
-        Payer payer = new Payer();
-        payer.setPaymentMethod("Paypal");
-
-        Payment payment = new Payment();
-        StringBuilder customUrl = new StringBuilder("?customerID=" + customerID + "&amount=" + total);
-        if (listAppointmentID != null) {
-            for (Integer appointmentID : listAppointmentID) {
-                customUrl.append("&appointmentID=").append(appointmentID);
-            }
-        }
-        payment.setIntent("sale");
-        payment.setPayer(payer);
-        payment.setTransactions(transactions);
-
-        RedirectUrls redirectUrls = new RedirectUrls();
-        redirectUrls.setCancelUrl(CANCEL_URL +"?customerID=" + customerID);
-        redirectUrls.setReturnUrl(SUCCESS_URL + customUrl);
-
-        payment.setRedirectUrls(redirectUrls);
-
-        return payment.create(apiContext);
     }
 }
