3ead299b82836c6acb10c9f49a414dc81ee162e7
aziky <pcm230304@gmail.com>
2024-06-16 00:37:27 +0700

    save payment to database

---
+++ src/main/java/com/team/controller/PaymentHistoryController.java
@@ -0,0 +1,10 @@
+package com.team.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment-history")
+public class PaymentHistoryController {
+
+}
