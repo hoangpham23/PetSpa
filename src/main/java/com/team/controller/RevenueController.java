e541704ecd32d61fed09c2645de20bb5a4699c65
TranQuocMinh <minhtqse183954@fpt.edu.vn>
2024-06-16 09:54:18 +0700

    DashBoard For admin

---
+++ src/main/java/com/team/controller/RevenueController.java
@@ -0,0 +1,20 @@
+package com.team.controller;
+
+import com.team.dto.RevenueDTO;
+import com.team.service.RevenueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RevenueController {
+    @Autowired
+    private RevenueService revenueService;
+
+    @GetMapping("/weekly-revenue")
+    public List<RevenueDTO> getWeeklyRevenue() {
+        return revenueService.getWeeklyRevenue();
+    }
+}
