1745e4d0d91a407c6bbe5e45aeb9dddef48bdcb0
zollie <thaonguyen07112004@gmail.com>
2024-06-22 02:08:43 +0700

    updateChooseServiceOnHeader

--- src/components/header/HeaderColor.js
+++ src/components/header/HeaderColor.js
@@ -1,28 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 //import "../../pages/home-page/HomeCus_style.css";
 // import "../../home-page/HomeGuest_style.css";
 import "./HeaderColor_style.css";
 function HeaderColor() {
+  const [name, setName] = useState("");
+  const dataArray = JSON.parse(localStorage.getItem("dataArray")) || [];
   useEffect(() => {
-    window.onscroll = () => {
+    const handleScroll = () => {
       let header = document.querySelector(".header");
-      header.classList.toggle("sticky", window.scrollY > 100);
+      if (header !== null) {
+        header.classList.toggle("sticky", window.scrollY > 100);
+      }
     };
-  });
-
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  useEffect(() => {
+    const accountData = localStorage.getItem("account");
+    if (accountData) {
+      const account = JSON.parse(accountData);
+      setName(account.customerName);
+    }
+  }, []);
   return (
     <header className="header">
-      <div className="header-blue-backgroud-rectangle"></div>
-      <div className="header-pink-backgroud-rectangle"></div>
+      <div className="header-blue-background-rectangle"></div>
+      <div className="header-pink-background-rectangle"></div>
       <div className="navbar-header">
         <nav className="navbar">
-          <a href="#">HOMEPAGE</a>
-          <a href="#">SERVICES</a>
+          <a href="/home-page">HOMEPAGE</a>
+          <div className="dropdown">
+            <a href="#service" className="dropbtn">
+              SERVICES
+            </a>
+            <div className="dropdown-content">
+              {dataArray.map((service, index) => (
+                <a
+                  key={index}
+                  href={`http://localhost:3000/home-page/${service.serviceName}`}
+                >
+                  {service.serviceName}
+                </a>
+              ))}
+            </div>
+          </div>
           <a href="#">CONTACT</a>
-          <a href="#">HISTORY PAYMENT</a>
+          <a href="/payment-history">HISTORY PAYMENT</a>
           <a href="/sign-in">SIGN OUT</a>
         </nav>
-        <p>Name</p>
+        <p>{name}</p>
       </div>
     </header>
   );
--- src/components/header/header-guest.js
+++ src/components/header/header-guest.js
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../pages/home-page/HomeGuest_style.css";
 function HeaderForGuest() {
-  window.onscroll = () => {
-    let header = document.querySelector(".header");
-    header.classList.toggle("sticky", window.scrollY > 100);
-  };
+  const dataArray = JSON.parse(localStorage.getItem("dataArray")) || [];
+  useEffect(() => {
+    const handleScroll = () => {
+      let header = document.querySelector(".header");
+      if (header !== null) {
+        header.classList.toggle("sticky", window.scrollY > 100);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  useEffect(() => {
+    console.log(dataArray, " neee");
+  }, [dataArray]);
   return (
     <header className="header">
       <div className="navbar-header">
         <nav className="navbar">
-          <a href="#">HOMEPAGE</a>
+          <a href="/home-page">HOMEPAGE</a>
           <div className="dropdown">
             <a href="#service" className="dropbtn">
               SERVICES
             </a>
             <div className="dropdown-content">
-              <a href="#">Service 1</a>
-              <a href="#">Service 2</a>
-              <a href="#">Service 3</a>
+              {dataArray.map((service, index) => (
+                <a
+                  key={index}
+                  href={`http://localhost:3000/home-page/${service.serviceName}`}
+                >
+                  {service.serviceName}
+                </a>
+              ))}
             </div>
           </div>
+
           <a href="#contact">CONTACT</a>
           <a href="/sign-in">SIGN IN</a>
         </nav>
-        <p>Name</p>
+        <p>GUEST</p>
       </div>
     </header>
   );
