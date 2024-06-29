4fe90c7ea931251a5cab7304f02df7c158585f7d
zollie <thaonguyen07112004@gmail.com>
2024-06-29 16:56:21 +0700

    addCompnentDashBoard

--- src/components/customCalendar/CustomDateRange.js
+++ src/components/customCalendar/CustomDateRange.js
@@ -1,30 +1,53 @@
-import * as React from "react";
-import dayjs from "dayjs";
-import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+// import * as React from "react";
+// import dayjs from "dayjs";
+// import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
-import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
+// import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 
-export default function DateRangePickerValue() {
-  //   const [value, setValue] = React.useState([
-  //     dayjs("2022-04-17"),
-  //     dayjs("2022-04-21"),
-  //   ]);
-  const today = dayjs();
-  const startOfMonth = today.startOf("month");
-  const endOfMonth = today.endOf("month");
-  const [value, setValue] = React.useState([startOfMonth, endOfMonth]);
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={["DateRangePicker", "DateRangePicker"]}>
-        <DemoItem component="DateRangePicker">
-          <DateRangePicker
-            value={value}
-            onChange={(newValue) => setValue(newValue)}
-          />
-        </DemoItem>
-      </DemoContainer>
-    </LocalizationProvider>
-  );
-}
+// export default function DateRangePickerValue() {
+//   const today = dayjs();
+//   const startOfMonth = formatDate(today.startOf("month"));
+//   const endOfMonth = formatDate(today.endOf("month"));
+//   const [value, setValue] = React.useState([startOfMonth, endOfMonth]);
+//   const [lineData, setLineData] = React.useState("[]");
+//   function formatDate(day) {
+//     if (!dayjs.isDayjs(day)) {
+//       throw new TypeError("Expected a dayjs object");
+//     }
+//     return day.format("YYYY-MM-DD");
+//   }
+//   const handleDateChange = (newValue) => {
+//     if (newValue && newValue[0] && newValue[1]) {
+//       const formattedStart = formatDate(dayjs(newValue[0]));
+//       const formattedEnd = formatDate(dayjs(newValue[1]));
+//       setValue([formattedStart, formattedEnd]);
+//     }
+//   };
+//   React.useEffect(() => {
+//     console.log("value ne", value);
+//   }, [value]);
+//   return (
+//     <LocalizationProvider dateAdapter={AdapterDayjs}>
+//       <DemoContainer components={["DateRangePicker", "DateRangePicker"]}>
+//         <DemoItem component="DateRangePicker">
+//           <DateRangePicker
+//             value={[dayjs(value[0]), dayjs(value[1])]}
+//             onChange={handleDateChange}
+//           />
+//         </DemoItem>
+//       </DemoContainer>
+//     </LocalizationProvider>
+//   );
+// }
+import React from "react";
+import { DatePicker, Space } from "antd";
+const { RangePicker } = DatePicker;
+const DateRangePickerValue = () => (
+  <Space direction="vertical" size={12}>
+    <RangePicker />
+    <RangePicker picker="month" />
+  </Space>
+);
+export default DateRangePickerValue;
