0c35686d7192b6917790943d4e4eb9aa19ba8e71
TranQuocMinh <minhtqse183954@fpt.edu.vn>
2024-06-30 21:40:47 +0700

    Manage Service Page for Admin

--- src/main/java/com/team/controller/AddServiceController.java
+++ src/main/java/com/team/controller/AddServiceController.java
@@ -1,21 +1,40 @@
 package com.team.controller;
 
 import com.team.dto.AddServiceDTO;
+import com.team.dto.ServiceImageDTO;
 import com.team.model.Services;
 import com.team.service.ServiceImagesService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/add-service")
 public class AddServiceController {
 
     private final ServiceImagesService serviceImagesService;
+    private final Logger logger = LoggerFactory.getLogger(HomePageImagesController.class);
+
 
     public AddServiceController(ServiceImagesService serviceImagesService) {
         this.serviceImagesService = serviceImagesService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllImages() {
+        try {
+            List<ServiceImageDTO> images = serviceImagesService.getHomePageImages();
+            return new ResponseEntity<>(images, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error in getting all images", e);
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @PostMapping
