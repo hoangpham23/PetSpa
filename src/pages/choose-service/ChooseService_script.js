8b00cdffcb2b2ae800254e76650818bdf003f02c
Hoang Pham <108130949+hoangpham23@users.noreply.github.com>
2024-07-22 20:27:16 +0700

    Delete src directory

--- src/pages/choose-service/ChooseService_script.js
+++
@@ -1,6 +0,0 @@
-// document.querySelectorAll('.service_img img').forEach(img => {
-//     img.addEventListener('click', function() {
-//         const checkbox = this.closest('.service_container').querySelector('input[type="checkbox"]');
-//         checkbox.checked = !checkbox.checked;
-//     });
-// });
