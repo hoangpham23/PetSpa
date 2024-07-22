6fa805cf5693ef7b529a8fcba35be16d2990acb3
Hoang Pham <108130949+hoangpham23@users.noreply.github.com>
2024-07-22 14:49:46 +0700

    Delete Server directory

--- Server/src/main/java/com/team/repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team.repository;
-
-import com.team.model.Pets;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetRepository  extends JpaRepository<Pets, Integer> {
-}
